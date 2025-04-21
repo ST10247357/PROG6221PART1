d43b79129ef39ebdce943b0541dd585c76f5d2bf
ST10247357 <st10247357@vcconnect.edu.za>
2025-04-21 23:41:14 +0200

    Implemented UserName class with a method to prompt for user name input, validate non-empty input, and handle exceptions for empty names.

---
+++ PROG6221PARTST10247357GIAJENICAGOUNDEN/UserName.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221PARTST10247357GIAJENICAGOUNDEN
+{
+    class UserName
+    {
+        public static string name;
+
+        public static void Name()
+        {
+
+
+            while (true)
+            {
+                try
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.DarkGray;// Changes the colour of the bot's input
+                    Console.WriteLine("Assistant: What is your name?");// Asks the user for their name
+                    Console.ResetColor();
+
+                    name = Console.ReadLine();
+
+                    // Check if the entered name is empty
+                    if (string.IsNullOrWhiteSpace(name))
+                    {
+
+                        throw new ArgumentException("Name cannot be empty. Please enter a valid name.");
+
+                    }
+
+                    break; // Exit loop if input is valid
+                }
+                catch (Exception ex)
+                {
+                    // Handle exception for empty name input
+                    Console.ForegroundColor = ConsoleColor.Red;// Changes the colour of the error message
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine($"Assistant: {ex.Message}");
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------------------------------");
+                    Console.WriteLine(""); // Leave a line
+                    Console.ResetColor();
+                }
+            }
+
+
+        }//End of UserName() method
+    }
+}
