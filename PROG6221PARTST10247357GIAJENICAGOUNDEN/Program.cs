6f1f0e3db7235680e284533f35fd928cde877464
ST10247357 <st10247357@vcconnect.edu.za>
2025-04-21 22:35:21 +0200

    Add project files.

---
+++ PROG6221PARTST10247357GIAJENICAGOUNDEN/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG6221PARTST10247357GIAJENICAGOUNDEN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
