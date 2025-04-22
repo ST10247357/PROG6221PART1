b6f52c3e6e02e7f0429c2fcaf86b1deada14b5b3
ST10247357 <st10247357@vcconnect.edu.za>
2025-04-22 12:18:33 +0200

    Added CyberSecurityTopics class with simple topic explanations and used it along with other intro classes in Main

--- PROG6221PARTST10247357GIAJENICAGOUNDEN/Program.cs
+++ PROG6221PARTST10247357GIAJENICAGOUNDEN/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Display the bot's logo
+            IntroductionUI.Logo();
+
+            // Play a welcome audio message
+            IntroductionUI.AudioWelcomeMessage();
+            Thread.Sleep(11000);// Pauses the current thread for 7000 milliseconds (which is equal to 7 seconds).
+            Console.WriteLine(""); // Leave a line
+
+            // Display the welcome message
+            IntroductionUI.WelcomeMessage();
+            Thread.Sleep(700);// Pauses the current thread for 700 milliseconds (which is equal to 0.7 seconds).
+            Console.WriteLine(""); // Leave a line
+
+            // Get the user's name
+            UserName.Name();
+            Thread.Sleep(700);// Pauses the current thread for 700 milliseconds (which is equal to 0.7 seconds).
+            Console.WriteLine(""); // Leave a line
         }
     }
 }
